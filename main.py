6961d0eab0cd526ed8bf3418836d05c5babc4018
Saidkhon <71948838+Saidhon4ik@users.noreply.github.com>
2026-04-18 22:36:18 +0500

    Implement user input for name and age
    Added user input functionality to greet the user and ask for their age.

---
+++ main.py
@@ -0,0 +1,28 @@
+# #####variables  
+
+# #String
+# first_name = "Bro"
+
+# #int
+# age = 20
+
+# #float 
+# pi = 3.14
+
+# #boolean
+# is_student = True
+
+# if is_student:
+#     print("You are a student")
+
+# else:
+#     print("You are not a student")
+
+
+
+
+#####user input
+name = input("what's your name?: ")
+age = (input("how old are u buddy?: "))
+print(f"sup {name}")
+print(f"You are {age} years old")
