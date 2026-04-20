7d675b0f1b9593ea9ea0a2574bde07bcd7098684
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-04-20 22:54:22 +0500

    updating all that stuff i forgot to push before....

--- main.py
+++ main.py
@@ -307,3 +307,59 @@
 
 
 
+# #####Consession stand program
+
+# menu = {"pizza" : 3.00,
+#         "nachos" : 4.50,
+#         "popcorn" : 6.00,
+#         "fries" : 2.50,
+#         "chips": 1.00,
+#         "pretzel": 3.50,
+#         "soda": 3.00,
+#         "lemonade": 4.25}
+
+
+
+# cart = []
+# total = 0
+
+# print("---------MENU------------")
+# for key,value in menu.items():
+#     print(f"{key:10}:${value:.2f}")
+# print("--------------------------")
+
+
+# while True:
+#     food = input("Select an item(q to quit): ").lower()
+#     if food == "q":
+#         break
+#     elif menu.get(food) is not None:
+#         cart.append(food)
+    
+# for food in cart:
+#     total += menu.get(food)
+#     print(food, end = " ")
+
+# print()
+# print(f"total is {total}")
+
+
+
+
+import random
+low = 1 
+high = 100
+
+options = ("rock","paper","scissors")
+cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+
+random.shuffle(cards)
+print(cards)
+
+# option = random.choice(options)
+
+# number = random.randint(low,high)
+# num = random.random()
+
+# print(number)
+# print(option) 
