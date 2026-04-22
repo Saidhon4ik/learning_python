e46e4eb19ca64d7915fac421cb42fc9267605376
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-04-22 22:30:34 +0500

    list comprehension

--- main.py
+++ main.py
@@ -619,3 +619,55 @@
 #     print("Valid email")
 # else:
 #     print("Invalid email")
+
+
+
+
+###list comprehension
+
+# #wrong
+# doubles = []
+# for x in range(1,11):
+#     doubles.append(x*2)
+    
+# print(doubles)
+
+
+#correct one
+# doubles = [x*2 for x in range(1,11)]
+# print(doubles)
+
+
+# triples = [y*3 for y in range(1,11)]
+# print(triples)
+
+
+# squares = [x**2 for x in range(1,11)]
+# print(squares)
+
+
+# fruits = ["apple","orange","banana","coconut"]
+# fruit_chars = [fruit[0] for fruit in fruits]
+
+# print(fruit_chars)  
+
+
+# numbers = [1,-2,3,-4,5,-6]
+
+# positive_nums = [num for num in numbers if num >= 0]
+# negative_nums = [num for num in numbers if num < 0]
+# even_nums = [num for num in numbers if num % 2 == 0]
+# odd_nums = [num for num in numbers if num % 2 == 1]
+
+# print(positive_nums)
+# print(negative_nums)
+# print(even_nums)
+
+
+
+
+
+# grades = [85,42,79,90,56,61,30]
+# passing_grades = [grade for grade in grades if grade >= 60]
+
+# print(passing_grades)
