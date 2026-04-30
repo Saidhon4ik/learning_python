2f574b6726411bd35831cb1913eb9db6dc8e6f55
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-01 01:06:02 +0500

    static methods

--- main.py
+++ main.py
@@ -1068,3 +1068,33 @@
 # #which requires a minimum necessary attributes or methods
 # # it is like, well, it can woof, it looks , sort of , like a dog, then... it is a dog?...
 # #my interpretation might be wrong tho... Tho it is my personal opinion on that
+
+
+
+# #####static method
+
+
+# class Employee:
+
+#     def __init__(self,name,position):
+#         self.name = name
+#         self.position = position
+
+#     def get_info(self):
+#         return f"{self.name} = {self.position}"
+    
+#     @staticmethod
+#     def is_valid_position(position):
+#         valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+#         return position in valid_positions
+    
+# employee1  = Employee("Eugene","Manager")
+# employee2 = Employee("Squidward", "Cashier")
+# employee3 = Employee("Spongebob", "Cook")
+
+
+# print(Employee.is_valid_position(position="Cook"))
+# print()
+# print(employee1.get_info())
+# print(employee2.get_info())
+# print(employee3.get_info())
