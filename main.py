91fc075dc9da123324e76704fe6dc1967c6c04a5
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-04-30 23:08:49 +0500

    duck typing in  python, sorta polymorphism

--- main.py
+++ main.py
@@ -997,45 +997,74 @@
 
 
 
-######Polymorphism
-from abc import ABC, abstractmethod
+# ######Polymorphism
+# from abc import ABC, abstractmethod
     
-class Shape(ABC):
-    @abstractmethod
-    def area(self):
-        pass
+# class Shape(ABC):
+#     @abstractmethod
+#     def area(self):
+#         pass
 
 
-class Circle(Shape):
-    def __init__(self,radius):
-        self.radius = radius
+# class Circle(Shape):
+#     def __init__(self,radius):
+#         self.radius = radius
 
-    def area(self):
-        return 3.14 * self.radius ** 2
+#     def area(self):
+#         return 3.14 * self.radius ** 2
 
-class Square(Shape):
-    def __init__(self,side):
-        self.side = side
+# class Square(Shape):
+#     def __init__(self,side):
+#         self.side = side
 
-    def area(self):
-        return self.side ** 2
+#     def area(self):
+#         return self.side ** 2
 
-class Triangle(Shape):  
-    def __init__(self,base, height):
-        self.base = base
-        self.height = height
+# class Triangle(Shape):  
+#     def __init__(self,base, height):
+#         self.base = base
+#         self.height = height
 
-    1
-    def area(self):
-        return self.base * self.height * 0.5
+#     1
+#     def area(self):
+#         return self.base * self.height * 0.5
 
-class Pizza(Circle):
-    def __init__(self,topping,radius):
-        super().__init__(radius)
-        self.topping = topping
+# class Pizza(Circle):
+#     def __init__(self,topping,radius):
+#         super().__init__(radius)
+#         self.topping = topping
 
 
-shapes = [Circle(4), Square(5), Triangle(3,4),Pizza("peperoni",5)]
+# shapes = [Circle(4), Square(5), Triangle(3,4),Pizza("peperoni",5)]
 
-for shape in shapes:
-    print(f"Area is {shape.area()} cm^2")
+# for shape in shapes:
+#     print(f"Area is {shape.area()} cm^2")
+
+
+
+# #####duck typing, for now i have no idea wth even is this thing gng
+# class Animal:
+#     alive = True
+
+# class Dog(Animal):
+#     def speak(self):
+#         print("Woof!")
+
+# class Cat(Animal):
+#     def speak(self):
+#         print("Meow!")
+
+# class Car:
+#     def speak(self):
+#         print("HONK!")
+
+# animals = [Dog(),Cat(),Car()]
+
+# for animal in animals:
+#     animal.speak()
+#     print(animal.alive)
+
+# #so, I would sum up all of that with the fact duck typing is sort of polymorphism
+# #which requires a minimum necessary attributes or methods
+# # it is like, well, it can woof, it looks , sort of , like a dog, then... it is a dog?...
+# #my interpretation might be wrong tho... Tho it is my personal opinion on that
