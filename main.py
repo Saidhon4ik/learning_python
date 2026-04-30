85b7065f3a14c9f8dbe42f518754d76ad342713c
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-04-30 18:39:43 +0500

    classes in python, Car.py is a file containing class Car for main.py

--- main.py
+++ main.py
@@ -742,58 +742,89 @@
 #functions and classes in this module can be reused without the main block of code executing
 
 
-####banking program
+# ####banking program
 
-def show_balance(balance):
-    print(f"your balance is ${balance:.2f}")
+# def show_balance(balance):
+#     print(f"your balance is ${balance:.2f}")
 
-def deposit(balance):
-    amount = float(input("Enter an amount to be deposited: "))
-    if amount < 0:
-        print("That is not a valid amount")
-        return 0
-    else:
-        return amount
+# def deposit(balance):
+#     amount = float(input("Enter an amount to be deposited: "))
+#     if amount < 0:
+#         print("That is not a valid amount")
+#         return 0
+#     else:
+#         return amount
 
-def withdraw(balance):
-    amount = float(input("Enter an amount to be withdrawn: "))
+# def withdraw(balance):
+#     amount = float(input("Enter an amount to be withdrawn: "))
 
-    if amount > balance:
-        print("Insufficient funds")
-        return 0
-    elif amount < 0:
-        print("Amount must be greater than 0")
-        return 0
-    else:
-        return amount
-
-
-def main():
-    balance = 0
-    is_running = True
+#     if amount > balance:
+#         print("Insufficient funds")
+#         return 0
+#     elif amount < 0:
+#         print("Amount must be greater than 0")
+#         return 0
+#     else:
+#         return amount
 
 
-    while is_running:
-        print("Python Banking program")
-        print("1. Show balance")
-        print("2. Deposit")
-        print("3. Withdraw")
-        print("4. Exit")
+# def main():
+#     balance = 0
+#     is_running = True
 
-        choice = input("Enter your choice(1-4): ")
-        if choice == "1":
-            show_balance(balance)
-        elif choice == "2":
-            balance += deposit(balance)
-        elif choice == "3":
-            balance -= withdraw(balance)
-        elif choice == "4":
-            is_running = False
-        else:
-            print("That is not a valid choice, try another one")
+
+#     while is_running:
+#         print("Python Banking program")
+#         print("1. Show balance")
+#         print("2. Deposit")
+#         print("3. Withdraw")
+#         print("4. Exit")
+
+#         choice = input("Enter your choice(1-4): ")
+#         if choice == "1":
+#             show_balance(balance)
+#         elif choice == "2":
+#             balance += deposit(balance)
+#         elif choice == "3":
+#             balance -= withdraw(balance)
+#         elif choice == "4":
+#             is_running = False
+#         else:
+#             print("That is not a valid choice, try another one")
     
-    print("Have a nice day")
+#     print("Have a nice day")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+#OOP in python
+from Car import Car
+car1 = Car("Mustang",2024,"Red",False)
+car2 = Car("Nexia",2016,"Blue",True)
+car3 = Car("Matiz",2012,"White",True)
+
+
+print(car1.color)
+print()
+
+
+
+car1.drive()
+car2.drive()
+car3.drive()
+
+
+print()
+car1.stop()
+car2.stop()
+car3.stop()
+
+
+print()
+car1.describe()
+car2.describe()
+car3.describe()
