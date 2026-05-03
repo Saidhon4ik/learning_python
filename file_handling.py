bdea7d722861c0e36cc5808a0bde6b45cda49bcd
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-03 16:14:09 +0500

    how to read or write any .txt, .json, or .csv files in python

--- file_handling.py
+++ file_handling.py
@@ -1,56 +1,56 @@
-# #===============================WRITING FILES =============== WRITING FILES=====================================
+#===============================WRITING FILES =============== WRITING FILES=====================================
 
-# #####for csv
-# import csv
+#####for csv
+import csv
 
-# employees = [["Name","Age","Job"],
-#              ["Spongebob",30,"cook"],
-#              ["Patrick",37,"Unemployed"],
-#              ["Sandy",27,"Scientist"]]
+employees = [["Name","Age","Job"],
+             ["Spongebob",30,"cook"],
+             ["Patrick",37,"Unemployed"],
+             ["Sandy",27,"Scientist"]]
 
-# file_path = "output.csv"
+file_path = "output.csv"
 
-# try:
-#     with open(file_path,"w", newline="") as file:
-#         writer = csv.writer(file)
-#         for row in employees:
-#             writer.writerow(row)
-#         print(f"csv file '{file_path}' was created")
-# except FileExistsError:
-#     print("File already exists, try again")
-
-
-
-# #for json
-# import json
-# employee = {
-#     "name": "Spongebob",
-#     "age" : 30,
-#     "job" : "cook"
-# }
-
-# file_path = "output.json"
-# try:
-#     with open(file= file_path, mode = "w") as file:
-#         json.dump(employee,file, indent= 4 )
-#         print(f"json file {file_path} was created ")
-# except FileExistsError:
-#     print("That file already exists")
+try:
+    with open(file_path,"w", newline="") as file:
+        writer = csv.writer(file)
+        for row in employees:
+            writer.writerow(row)
+        print(f"csv file '{file_path}' was created")
+except FileExistsError:
+    print("File already exists, try again")
 
 
 
+#for json
+import json
+employee = {
+    "name": "Spongebob",
+    "age" : 30,
+    "job" : "cook"
+}
+
+file_path = "output.json"
+try:
+    with open(file= file_path, mode = "w") as file:
+        json.dump(employee,file, indent= 4 )
+        print(f"json file {file_path} was created ")
+except FileExistsError:
+    print("That file already exists")
 
 
-# #for txt
-# txt_data = "I like pizza"
 
-# file_path = "output.txt"
-# try:
-#     with open(file_path, "w") as file:
-#         file.write(txt_data)
-#         print("File has been created")
-# except FileExistsError:
-#     print(f"File '{file_path}' already exists")
+
+
+#for txt
+txt_data = "I like pizza"
+
+file_path = "output.txt"
+try:
+    with open(file_path, "w") as file:
+        file.write(txt_data)
+        print("File has been created")
+except FileExistsError:
+    print(f"File '{file_path}' already exists")
 
 
 
