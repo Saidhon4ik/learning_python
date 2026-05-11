a5d972083ba13189455c77944f610042e7ba37bf
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-11 15:58:33 +0500

    got two questions, one of which i did on github

--- tasks.py
+++ tasks.py
@@ -1,11 +1,43 @@
-#1
-def select_second(L):
-    """Return the second element of the given list. If the list has no second
-    element, return None.
-    """
-    if len(L) > 1:
-        return L[1]
-    else:
-        return None
+# #1
+# def select_second(L):
+#     """Return the second element of the given list. If the list has no second
+#     element, return None.
+#     """
+#     if len(L) > 1:
+#         return L[1]
+#     else:
+#         return None
 
-print(select_second(L=[[1]]))
+# print(select_second(L=[[1]]))
+
+
+
+
+
+
+# class Solution:
+#     def mySqrt(self,x):
+#        L,R = 1, x
+#        while L <= R:
+#               M = (L+R)//2
+#               M_squared = M*M
+
+#        if M_squared == x:
+#               return M
+#        elif M_squared < x:
+#               L = M+ 1
+#        else: 
+#               R = M - 1
+#        return R
+
+
+
+
+
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
