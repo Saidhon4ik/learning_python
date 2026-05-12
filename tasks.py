a140b2cd4a31698fad67fd7b222d1b4d1fc4e017
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-12 21:01:04 +0500

    updating tasks.py, adding some more samples

--- tasks.py
+++ tasks.py
@@ -14,7 +14,7 @@
 
 
 
-
+#that was from the video, reels from the instagram
 # class Solution:
 #     def mySqrt(self,x):
 #        L,R = 1, x
@@ -34,7 +34,36 @@
 
 
 
+#https://leetcode.com/problems/two-sum/
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
 
+
+
+
+
+#https://leetcode.com/problems/palindrome-number/
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x < 0 or (x % 10 == 0 and x != 0):
+#             return False
+#         n = x
+#         result = 0
+#         while n > 0:
+#             result = result * 10 + n % 10
+#             n//=10
+#         return x == result
+# #that was hard gng...
+
+
+
+
+
+#https://leetcode.com/problems/two-sum/
 # class Solution(object):
 #     def twoSum(self, nums, target):
 #         for i in range(len(nums)):
