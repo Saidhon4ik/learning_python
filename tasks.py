c671ab8a84eae61af0b209b0c797c8021b4e6144
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-13 19:41:32 +0500

    just adding an example of binary search in python

--- tasks.py
+++ tasks.py
@@ -87,3 +87,29 @@
         
 #         if y in h and h[y]  != i:
 #             return [i,h[y]]
+
+
+
+#example of a binary search
+def binary_search(list,target):
+        low = 0
+        high = len(list) - 1
+
+        while low <= high:
+                mid = (low+high)//2     
+                guess = list[mid]
+
+                if guess == target:
+                        return mid
+                elif guess > target:
+                        high = mid - 1
+                elif guess < target:
+                        low = mid + 1
+        return None
+
+my_list = [1,3,5,7,9]
+
+
+print(binary_search(my_list,1))
+print(binary_search(my_list,-1))
+print(binary_search(my_list,3))
